191d317b0ec792cce21e8b62004f443a8e7690a2
Camleaf <bdaace09@gmail.com>
2025-09-17 16:26:12 -0400

    User starting loop works

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 import pkg.board.Board;
 import pkg.board.Ship;
 import pkg.utils.Utils;
-import pkg.utils.Utils.*;
-
-import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.Exception;
 
@@ -11,21 +8,16 @@ public class Main {
     Board pBoard; // player board
     Board aBoard; // bot board
     Scanner input;
+    public static void Clear(){System.out.print("\033\143");}
+
     public static void main(String[] args) throws Exception{
         Main main = new Main();
-        
     }
 
     public Main() throws Exception{
         this.pBoard = new Board();
         this.aBoard = new Board();
         this.input = new Scanner(System.in);
-        // board.displayDefense(Board.emptyIntInt);
-        // int[][] arr = {{0,0},{0,1},{1,1}};
-        // board.displayDefense(arr);
-
-        // int[][] newShip = {{6,5},{6,6},{6,7}};
-        // board.createShip(newShip, 3, 0);
         // board.displayDefense(arr);
         // int[] cast = {6,5};
         // board.attack(cast);
@@ -43,26 +35,28 @@ public class Main {
         this.initializeBoards();
 
     }
+    public boolean checkBoundaries(int x,int y){
+        return x <= 9 && x >= 0 && y <= 9 && y >= 0;
+    }
     public void initializeBoards(){
-
+        int shipNumber = 0;
         for (int length : Ship.lengths){
+            boolean playerChoosing = true;
             int[][] curShip = new int[length][2];
             for (int index = 0; index < length; index++){
                 curShip[index][1] = index;
             }
-            while (true){
+            while (playerChoosing){
+                // Takes input from player, moves ship accordingly
                 this.pBoard.displayDefense(curShip);
                 System.out.println("wsad for movement, enter to submit each char. c to confirm, r to rotate");
                 char instruction;
                 try{
                     instruction = input.nextLine().toLowerCase().charAt(0);
-                } catch (IndexOutOfBoundsException e){continue;}
+                } catch (IndexOutOfBoundsException e){Clear();continue;}
 
                 // init new ship
-                int[][] newShip = new int[length][2];
-                for (int i=0;i<curShip.length;i++){
-                    newShip[i] = curShip[i].clone();
-                }
+                int[][] newShip = Utils.deepCopy(curShip);
 
                 switch (instruction){
                     case 'w':
@@ -80,22 +74,74 @@ public class Main {
                             newShip[i][1] += 1;
                         }
                         break;
-                    case 'a':
+                    case 'a': 
                         for (int i = 0; i < curShip.length;i++){
                             newShip[i][1] -= 1;
                         }
                         break;
+                    case 'c':
+                        boolean isMatch = false;
+                        for (int[] pos : newShip){
+                            if (pBoard.isShipMatch(pos)){
+                                isMatch = true;
+                                break;
+                            }
+                        }
+                        if (!isMatch){
+                            pBoard.createShip(newShip, newShip.length, shipNumber);
+                            playerChoosing = false;
+                        }
+                        break;
+                    case 'r': //Rotation
+                        int[] rotationCenter = newShip[0].clone();
+                        int[][] rotationPoints = new int[length][2];
+                        int[][] antiRotationPoints = new int[length][2];
+
+                        for (int i = 0;i<newShip.length;i++){
+                            // center coords around head of ship, then inverse x and y to rotate around new origin, then add back coords of center to get the rotated ship.
+                            int[] pos = {newShip[i][1]-rotationCenter[1]+rotationCenter[0],newShip[i][0]-rotationCenter[0]+rotationCenter[1]};
+                            int[] antipos = {-(newShip[i][1]-rotationCenter[1])+rotationCenter[0],-(newShip[i][0]-rotationCenter[0])+rotationCenter[1]};
+                            rotationPoints[i] = pos.clone();
+                            antiRotationPoints[i] = antipos.clone();
+                        }
+
+                        boolean f = true;
+                        for (int i = 0; i < curShip.length;i++){
+                            if (!checkBoundaries(rotationPoints[i][0], rotationPoints[i][1])){
+                                f = false;
+                                break;
+                            }   
+                        }
+                        if (f){newShip=rotationPoints;break;}
+                        f = true;
+                        // use antipos for counterclockwise rotation
+                        for (int i = 0; i < curShip.length;i++){
+                            if (!checkBoundaries(antiRotationPoints[i][0], antiRotationPoints[i][1])){
+                                f = false;
+                                break;
+                            }   
+                        }
+                        if (f){newShip=Utils.reverseArray(antiRotationPoints);break;}
+                        break;
+
+
+
+
+                        // checkBoundaries(newShip[i][0], newShip[i][1]);
                 }
+
                 boolean b = true;
                 for (int i = 0; i < curShip.length;i++){
-                    if (newShip[i][0] > 9 || newShip[i][0] < 0 || newShip[i][1] > 9 || newShip[i][1] < 0){
+                    if (!checkBoundaries(newShip[i][0], newShip[i][1])){
                         b = false;
                         break;
                     }
                 }
+
                 if (b){curShip = newShip;}
-                System.out.print("\033\143");
+                Clear();
             }
+            shipNumber += 1;
 
         }
     }
--- pkg/utils/Utils.java
+++ pkg/utils/Utils.java
@@ -12,5 +12,33 @@ public class Utils {
 
         return false;
     }
+
+    public static int[][] deepCopy(int[][] arr) {
+        int[][] newArr = new int[arr.length][];
+        for (int index = 0; index < arr.length; index++){
+            newArr[index] = arr[index].clone();
+        }
+        return newArr;
+    }
+
+    public static int[][] reverseArray(int[][] arr){
+        /**
+         * Returns a new array which is the reverse of the input array. 
+         * */
+        int[][] sortedArr = new int[arr.length][];
+        for (int i = 0;i<arr.length;i++){
+            int[] element = arr[i];
+            sortedArr[i] = element.clone();
+        }
+        return arr;
+    }
+    public static int[] reverseArray(int[] arr){
+        int[] sortedArr = new int[arr.length];
+        for (int i = 0;i<arr.length;i++){
+            int element = arr[i];
+            sortedArr[i] = element;
+        }
+        return sortedArr;
+    }
 }
 
