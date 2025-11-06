aff525f02d13fe73326c4ff6eb9b289d3ce0059e
Camleaf <bdaace09@gmail.com>
2025-11-06 09:03:30 -0500

    do this later

--- U3/main/game/board/Piece.java
+++ U3/main/game/board/Piece.java
@@ -4,11 +4,40 @@ public class Piece {
     public static enum Type {
         PAWN(0), BISHOP(1), KING(2), KNIGHT(3), QUEEN(4), ROOK(5);
         public int id;
-        private Type(int index){this.id = index;}
+        private Type(int index){
+            this.id = index;
+        }
     }
+
+
     public static enum Colour {
         WHITE(0),BLACK(1);
         public int id;
         private Colour(int index){this.id = index;}
     }
+
+
+    private int x;
+    private int y;
+    private Type type;
+    private Colour colour;
+    private Boolean hasMoved;
+
+    public Piece(int x, int y, Type type, Colour colour){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.colour = colour;
+        this.hasMoved = false;
+    }
+
+    public Piece(int x, int y, Type type, Colour colour, boolean hasMoved){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.colour = colour;
+        this.hasMoved = hasMoved;
+    }
+
+    
 }
